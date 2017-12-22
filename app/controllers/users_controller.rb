d81948f45b04175060fb3ebd30a1182150883d9b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-22 03:17:34 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    if @user = User.find_by(username: user_params[:username])
+      return head(:forbidden) unless @user.authenticate(user_params[:password])
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
