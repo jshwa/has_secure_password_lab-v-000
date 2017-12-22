43ba41532fd4a1f67f7dea0a5a52f229684b2b97
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-22 03:13:09 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def index
+    if logged_in
+
+    else
+      redirect_to '/users/new'
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
 end
