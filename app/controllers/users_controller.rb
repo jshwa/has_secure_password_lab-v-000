abcf84e35d537119ca00afa876ac51c1ef692268
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-22 03:40:06 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+    @user = User.create(user_params)
     session[:user_id] = @user.id
     redirect_to 'users/index'
   end
